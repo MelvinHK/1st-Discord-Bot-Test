43879cec92b504926d52837858984cac3743f503
MelvinHK <53848567+MelvinHK@users.noreply.github.com>
2020-12-04 00:44:07 +1100

    made ping/pong message

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+"use strict";
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 require('dotenv').config();
 
 client.login(process.env.BOT_TOKEN);
+
+client.on('ready', () => {
+    console.log("I'm ready.");
+});
+
+client.on('message', msg => {
+    if (msg.content === '!ping') {
+        msg.channel.send('pong!');
+    }
+});
