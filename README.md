482254b9ca8917f1fe33778505d6bc4f4a6fa2f1
MelvinHK <53848567+MelvinHK@users.noreply.github.com>
2020-12-05 01:13:52 +1100

    edited command arguments, added !cat command

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 1st-Test-Discord-Bot
 First Discord bot I made for testing stuff
 
-You must add your own .env file with the environmental variable:
+Install dotenv and add an .env file with the environmental variable:
 
 BOT_TOKEN = 'your Discord bot token'
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fetch = require('node-fetch');
 require('dotenv').config();
 
 client.login(process.env.BOT_TOKEN);
@@ -12,12 +13,25 @@ client.on('ready', () => {
     console.log("I'm ready.");
 });
 
-client.on('message', msg => {
-    if (msg.content.startsWith(`${prefix}ping`)) {
+client.on('message', async msg => {
+    if (!msg.content.startsWith(prefix)) return;
+
+    const args = msg.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping' && !args.length) {
         const embed = new Discord.MessageEmbed()
             .setTitle('🏓 pong!')
             .setColor('#FF0000')
-            .setDescription(`${Date.now() - msg.createdTimestamp}ms`)
+            .addFields(
+                { name: 'your ping:', value: `${Date.now() - msg.createdTimestamp}ms`, inline: true }
+            );
         msg.channel.send(embed);
+    }
+
+    if (command === 'cat' && !args.length) {
+        const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
+        msg.channel.send('here is a random cat image for you');
+        msg.channel.send(file);
     }
 });
